61d7998648451bd8f6f302387e65781afd2ec7e2
Francisco Brito <francisco.brito.ac@gmail.com>
2021-01-14 11:27:15 -0300

    DTO Category

--- backend/src/main/java/com/facbrito/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/facbrito/dscatalog/services/CategoryService.java
@@ -1,11 +1,14 @@
 package com.facbrito.dscatalog.services;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.facbrito.dscatalog.dto.CategoryDTO;
 import com.facbrito.dscatalog.entities.Category;
 import com.facbrito.dscatalog.repositories.CategoryRepository;
 
@@ -14,10 +17,11 @@ public class CategoryService {
 
 	@Autowired
 	private CategoryRepository repository;
-	
+
 	@Transactional(readOnly = true)
-	public List<Category> findAll() {
-		return repository.findAll();
+	public List<CategoryDTO> findAll() {
+		List<Category> list = repository.findAll();
+		return list.stream().map(c -> new CategoryDTO(c)).collect(Collectors.toList());
 	}
-	
+
 }
