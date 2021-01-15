6b9edca6afebcf932c2cdfa8b0a82fce83256163
Francisco Brito <francisco.brito.ac@gmail.com>
2021-01-14 23:25:14 -0300

    Delete category

--- backend/src/main/java/com/facbrito/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/facbrito/dscatalog/services/CategoryService.java
@@ -6,12 +6,15 @@ import java.util.stream.Collectors;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.facbrito.dscatalog.dto.CategoryDTO;
 import com.facbrito.dscatalog.entities.Category;
 import com.facbrito.dscatalog.repositories.CategoryRepository;
+import com.facbrito.dscatalog.services.exceptions.DatabaseException;
 import com.facbrito.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -50,6 +53,18 @@ public class CategoryService {
 			return new CategoryDTO(category);
 		} catch (EntityNotFoundException e) {
 			throw new ResourceNotFoundException("Id not found " + id);
+		}
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} 
+		catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found" + id);
+		} 
+		catch(DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
 		}
 	}
 
